7e919545441ebdae442ac5fc0a28f19978c416bb
lisandroschneider <lisandroschneider7@gmail.com>
2022-10-13 18:25:00 -0300

    subir archivo

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ejercicio_n_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            int entero = 1;
+            double n1 = 1.12;
+            bool logico = true;
+            string cadenaTexto = "hola";
+
+            Console.WriteLine(entero);
+            Console.WriteLine(n1);
+            Console.WriteLine(logico);
+            Console.WriteLine(cadenaTexto);
+        }
+    }
+}
